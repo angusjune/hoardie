fea628e4bcb0ca8c40566b458695facbcd804623
Angus Zhu <angus.is.online@gmail.com>
2021-05-11 09:49:01 +0800

    Updated

--- src/background.js
+++ src/background.js
@@ -11,15 +11,52 @@ chrome.action.onClicked.addListener(async tab => {
   });
 
   // save opened tabs in storage
-  await chrome.storage.local.set({ tabs: tabs });
+  await setTabsData(tabs);
 
   // close all tabs
   // await chrome.tabs.remove(ids);
 
-  // open homepage
-  chrome.tabs.create({
-    url: 'index.html',
-    pinned: true,
-  });
+  chrome.storage.local.get(['indexId'], props => {
+    const indexId = props.indexId;
 
+    if (indexId === undefined) {
+      // open homepage
+      chrome.tabs.create({
+        url: 'index.html',
+        pinned: true,
+      }, tab => {
+        console.log(tab.id);
+        chrome.storage.local.set({ indexId: tab.id })
+      });
+    } else {
+      try {
+        // update homepage
+        chrome.tabs.update(indexId, {
+          url: 'index.html',
+          pinned: true
+        });
+      } catch (e) {
+        console.error(e);
+        chrome.storage.local.remove(['indexId']);
+      }
+    }
+  });  
+});
+
+function setTabsData(tabs) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.set({ tabs: tabs }, () => {
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError);
+      }
+      resolve();
+    });
+  })
+}
+
+chrome.tabs.onRemoved.addListener(async (tabId, info) => {
+  const indexId = await chrome.storage.local.get(['indexId']);
+  if (tabId === indexId) {
+    chrome.storage.local.remove(['indexId']);
+  }
 });
